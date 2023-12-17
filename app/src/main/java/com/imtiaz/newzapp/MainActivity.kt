d6d4fcfc3c822d026414997eb1d37d5e5e1643b9
ibnul.imtiaz <imtiazeemel42@gmail.com>
2023-12-17 18:12:21 +0600

    SplashScreenAPI

---
+++ app/src/main/java/com/imtiaz/newzapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.imtiaz.newzapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.imtiaz.newzapp.ui.theme.NewzAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContent {
+            NewzAppTheme {
+
+            }
+        }
+    }
+}
+
+
